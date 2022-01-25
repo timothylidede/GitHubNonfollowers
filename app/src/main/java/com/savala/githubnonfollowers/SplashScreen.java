1fa1e1423afe592fc8beb3a730cda7933083a032
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-26 02:42:56 +0300

    splash screen timeout

--- app/src/main/java/com/savala/githubnonfollowers/SplashScreen.java
+++ app/src/main/java/com/savala/githubnonfollowers/SplashScreen.java
@@ -2,10 +2,14 @@ package com.savala.githubnonfollowers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,14 @@ public class SplashScreen extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(SplashScreen.this, Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
