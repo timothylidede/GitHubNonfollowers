71a247f29bdd44fc558b39d50a387163aa5fa437
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-26 14:15:13 +0300

    initializing sign in button

--- app/src/main/java/com/savala/githubnonfollowers/Home.java
+++ app/src/main/java/com/savala/githubnonfollowers/Home.java
@@ -2,18 +2,27 @@ package com.savala.githubnonfollowers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
-    private Button mSignUp;
+    private Button mSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mSignUp
+        mSignIn = (Button) findViewById(R.id.sign_in_button);
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
